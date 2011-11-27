9bc289b2cf8066f01b1b69f3b88918b1ffbb1f78
Tomasz Siekierda (sierdzio) <sierdzio@gmail.com>
2011-11-27 15:19:52 +0100

    Add default config file and make it a fallback in case of errors.
    When parsing of main config file fails, default one will be used instead.

--- base/main.cpp
+++ base/main.cpp
@@ -1,7 +1,7 @@
 #include <QtGui/QApplication>
 #include <QtDeclarative/QDeclarativeView>
 #include <QtDeclarative/QDeclarativeContext>
-#include <QDebug>
+//#include <QDebug>
 #include "ccfconfig.h"
 
 int main(int argc, char *argv[])
@@ -9,20 +9,27 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QDeclarativeView *viewer = new QDeclarativeView();
 
-    CcfConfig configuration("config");
-    if (configuration.isErrorState()) {
-        qDebug() << "Error while reading configuration! Message: "
-                 << configuration.errorMessage();
-    }/* else {
-        qDebug() << configuration.configurationString();
-    }*/
-    viewer->rootContext()->setContextObject(&configuration);
+    CcfConfig *configuration = new CcfConfig("config");
+    if (configuration->isErrorState()) {
+        printf("Error while reading configuration! Message: "
+               + configuration->errorMessage().toLocal8Bit() + "\n");
+        printf("Loading default configuration... ");
+        configuration = new CcfConfig("config_default");
+
+        if (configuration->isErrorState()) {
+            printf("ERROR: " + configuration->errorMessage().toLocal8Bit() + "\n");
+            return 1;
+        } else {
+            printf("OK\n");
+        }
+    }
+    viewer->rootContext()->setContextObject(configuration);
 
     QString pwd = a.applicationDirPath() + "/";
     viewer->rootContext()->setContextProperty("PWD", pwd);
 
-    viewer->rootContext()->setContextProperty("windowWidth", configuration.configWindowWidth());
-    viewer->rootContext()->setContextProperty("windowHeight", configuration.configWindowHeight());
+    viewer->rootContext()->setContextProperty("windowWidth", configuration->configWindowWidth());
+    viewer->rootContext()->setContextProperty("windowHeight", configuration->configWindowHeight());
 
     viewer->setSource(QUrl("base/main.qml"));
     viewer->setAttribute(Qt::WA_OpaquePaintEvent);
