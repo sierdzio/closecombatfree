e6099e9e0b2e19997097303c1ac5788a0af02c30
Tomasz Siekierda (sierdzio) <sierdzio@gmail.com>
2012-08-22 13:34:07 +0200

    CcfQmlBaseScenario has all it's properties defined. Dumb copy job. Compiles.

--- src/qmlBase/ccfqmlbasescenario.cpp
+++ src/qmlBase/ccfqmlbasescenario.cpp
@@ -4,3 +4,241 @@ CcfQmlBaseScenario::CcfQmlBaseScenario(QQuickItem *parent) :
     QQuickItem(parent)
 {
 }
+
+QString CcfQmlBaseScenario::getScenarioFile()
+{
+    return m_scenarioFile;
+}
+
+QString CcfQmlBaseScenario::getScenarioWinStatus()
+{
+    return m_scenarioWinStatus;
+}
+
+int CcfQmlBaseScenario::getAimLineRotation()
+{
+    return m_aimLineRotation;
+}
+
+int CcfQmlBaseScenario::getRubberBandRotation()
+{
+    return m_rubberBandRotation;
+}
+
+int CcfQmlBaseScenario::getUnitIndex()
+{
+    return m_unitIndex;
+}
+
+bool CcfQmlBaseScenario::getIsCampaign()
+{
+    return m_isCampaign;
+}
+
+QString CcfQmlBaseScenario::getMapFile()
+{
+    return m_mapFile;
+}
+
+QList<QObject *> CcfQmlBaseScenario::getEffectsContainer()
+{
+    return m_effectsContainer;
+}
+
+QList<QObject *> CcfQmlBaseScenario::getOrderMarkersContainer()
+{
+    return m_orderMarkersContainer;
+}
+
+QList<QObject *> CcfQmlBaseScenario::getUnitGroups()
+{
+    return m_unitGroups;
+}
+
+qreal CcfQmlBaseScenario::getZoom()
+{
+    return m_zoom;
+}
+
+QPoint CcfQmlBaseScenario::getZoomPoint()
+{
+    return m_zoomPoint;
+}
+
+bool CcfQmlBaseScenario::getPaused()
+{
+    return m_paused;
+}
+
+QColor CcfQmlBaseScenario::getMenuBackgroundColor()
+{
+    return m_menuBackgroundColor;
+}
+
+void CcfQmlBaseScenario::setScenarioFile(const QString &scenarioFile)
+{
+    bool wasChaged = false;
+    if (scenarioFile != m_scenarioFile)
+        wasChaged = true;
+
+    m_scenarioFile = scenarioFile;
+
+    if (wasChaged)
+        emit scenarioFileChanged();
+}
+
+void CcfQmlBaseScenario::setScenarioWinStatus(const QString &scenarioWinStatus)
+{
+    bool wasChaged = false;
+    if (scenarioWinStatus != m_scenarioWinStatus)
+        wasChaged = true;
+
+    m_scenarioWinStatus = scenarioWinStatus;
+
+    if (wasChaged)
+        emit scenarioWinStatusChanged();
+}
+
+void CcfQmlBaseScenario::setAimLineRotation(int aimLineRotation)
+{
+    bool wasChaged = false;
+    if (aimLineRotation != m_aimLineRotation)
+        wasChaged = true;
+
+    m_aimLineRotation = aimLineRotation;
+
+    if (wasChaged)
+        emit aimLineRotationChanged();
+}
+
+void CcfQmlBaseScenario::setRubberBandRotation(int rubberBandRotation)
+{
+    bool wasChaged = false;
+    if (rubberBandRotation != m_rubberBandRotation)
+        wasChaged = true;
+
+    m_rubberBandRotation = rubberBandRotation;
+
+    if (wasChaged)
+        emit rubberBandRotationChanged();
+}
+
+void CcfQmlBaseScenario::setUnitIndex(int unitIndex)
+{
+    bool wasChaged = false;
+    if (unitIndex != m_unitIndex)
+        wasChaged = true;
+
+    m_unitIndex = unitIndex;
+
+    if (wasChaged)
+        emit unitIndexChanged();
+}
+
+void CcfQmlBaseScenario::setIsCampaign(bool isCampaign)
+{
+    bool wasChaged = false;
+    if (isCampaign != m_isCampaign)
+        wasChaged = true;
+
+    m_isCampaign = isCampaign;
+
+    if (wasChaged)
+        emit isCampaignChanged();
+}
+
+void CcfQmlBaseScenario::setMapFile(const QString &mapFile)
+{
+    bool wasChaged = false;
+    if (mapFile != m_mapFile)
+        wasChaged = true;
+
+    m_mapFile = mapFile;
+
+    if (wasChaged)
+        emit mapFileChanged();
+}
+
+void CcfQmlBaseScenario::setEffectsContainer(const QList<QObject *> &effectsContainer)
+{
+    bool wasChaged = false;
+    if (effectsContainer != m_effectsContainer)
+        wasChaged = true;
+
+    m_effectsContainer = effectsContainer;
+
+    if (wasChaged)
+        emit effectsContainerChanged();
+}
+
+void CcfQmlBaseScenario::setOrderMarkersContainer(const QList<QObject *> &orderMarkersContainer)
+{
+    bool wasChaged = false;
+    if (orderMarkersContainer != m_orderMarkersContainer)
+        wasChaged = true;
+
+    m_orderMarkersContainer = orderMarkersContainer;
+
+    if (wasChaged)
+        emit orderMarkersContainerChanged();
+}
+
+void CcfQmlBaseScenario::setUnitGroups(const QList<QObject *> &unitGroups)
+{
+    bool wasChaged = false;
+    if (unitGroups != m_unitGroups)
+        wasChaged = true;
+
+    m_unitGroups = unitGroups;
+
+    if (wasChaged)
+        emit unitGroupsChanged();
+}
+
+void CcfQmlBaseScenario::setZoom(qreal zoom)
+{
+    bool wasChaged = false;
+    if (zoom != m_zoom)
+        wasChaged = true;
+
+    m_zoom = zoom;
+
+    if (wasChaged)
+        emit zoomChanged();
+}
+
+void CcfQmlBaseScenario::setZoomPoint(const QPoint &zoomPoint)
+{
+    bool wasChaged = false;
+    if (zoomPoint != m_zoomPoint)
+        wasChaged = true;
+
+    m_zoomPoint = zoomPoint;
+
+    if (wasChaged)
+        emit zoomPointChanged();
+}
+
+void CcfQmlBaseScenario::setPaused(bool paused)
+{
+    bool wasChaged = false;
+    if (paused != m_paused)
+        wasChaged = true;
+
+    m_paused = paused;
+
+    if (wasChaged)
+        emit pausedChanged();
+}
+
+void CcfQmlBaseScenario::setMenuBackgroundColor(const QColor &menuBackgroundColor)
+{
+    bool wasChaged = false;
+    if (menuBackgroundColor != m_menuBackgroundColor)
+        wasChaged = true;
+
+    m_menuBackgroundColor = menuBackgroundColor;
+
+    if (wasChaged)
+        emit menuBackgroundColorChanged();
+}
--- src/qmlBase/ccfqmlbasescenario.h
+++ src/qmlBase/ccfqmlbasescenario.h
@@ -1,6 +1,9 @@
 #ifndef CCFQMLBASESCENARIO_H
 #define CCFQMLBASESCENARIO_H
 
+#include <QtCore/QString>
+#include <QtCore/QList>
+#include <QtCore/QPoint>
 #include <QtQuick/QQuickItem>
 
 /*!
@@ -11,8 +14,98 @@
 class CcfQmlBaseScenario : public QQuickItem
 {
     Q_OBJECT
+
+    // Many of those can be changed into local vars, probalby
+    Q_PROPERTY(QString scenarioFile READ getScenarioFile WRITE setScenarioFile NOTIFY scenarioFileChanged)
+    Q_PROPERTY(QString scenarioWinStatus READ getScenarioWinStatus WRITE setScenarioWinStatus NOTIFY scenarioWinStatusChanged)
+    Q_PROPERTY(int aimLineRotation READ getAimLineRotation WRITE setAimLineRotation NOTIFY aimLineRotationChanged)
+    Q_PROPERTY(int rubberBandRotation READ getRubberBandRotation WRITE setRubberBandRotation NOTIFY rubberBandRotationChanged)
+    Q_PROPERTY(int unitIndex READ getUnitIndex WRITE setUnitIndex NOTIFY unitIndexChanged)
+    // Is this a single scenario, or a campaign? Convenience bool (same info can
+    // be taken from other sources
+    Q_PROPERTY(bool isCampaign READ getIsCampaign WRITE setIsCampaign NOTIFY isCampaignChanged)
+    // Map path for campaigns
+    Q_PROPERTY(QString mapFile READ getMapFile WRITE setMapFile NOTIFY mapFileChanged)
+    // Holds graphical effects (muzzle flashes, hit animations etc.).
+    Q_PROPERTY(QList<QObject *> effectsContainer READ getEffectsContainer WRITE setEffectsContainer NOTIFY effectsContainerChanged)
+    // Holds order markers.
+    Q_PROPERTY(QList<QObject *> orderMarkersContainer READ getOrderMarkersContainer WRITE setOrderMarkersContainer NOTIFY orderMarkersContainerChanged)
+    // Holds unit groups (ones created with CTRL + digit).
+    Q_PROPERTY(QList<QObject *> unitGroups READ getUnitGroups WRITE setUnitGroups NOTIFY unitGroupsChanged)
+    Q_PROPERTY(qreal zoom READ getZoom WRITE setZoom NOTIFY zoomChanged)
+    Q_PROPERTY(QPoint zoomPoint READ getZoomPoint WRITE setZoomPoint NOTIFY zoomPointChanged)
+    Q_PROPERTY(bool paused READ getPaused WRITE setPaused NOTIFY pausedChanged)
+    Q_PROPERTY(QColor menuBackgroundColor READ getMenuBackgroundColor WRITE setMenuBackgroundColor NOTIFY menuBackgroundColorChanged)
+
+//    property point zoomPoint: Qt.point(gameArea.width/2 + gameArea.contentX,
+//                                       gameArea.height/2 + gameArea.contentY); //Qt.point(0, 0)
+
 public:
     explicit CcfQmlBaseScenario(QQuickItem *parent = 0);
+
+    // Property getters:
+    QString getScenarioFile();
+    QString getScenarioWinStatus();
+    int getAimLineRotation();
+    int getRubberBandRotation();
+    int getUnitIndex();
+    bool getIsCampaign();
+    QString getMapFile();
+    QList<QObject *> getEffectsContainer();
+    QList<QObject *> getOrderMarkersContainer();
+    QList<QObject *> getUnitGroups();
+    qreal getZoom();
+    QPoint getZoomPoint();
+    bool getPaused();
+    QColor getMenuBackgroundColor();
+
+    //Property setters:
+    void setScenarioFile(const QString &scenarioFile);
+    void setScenarioWinStatus(const QString &scenarioWinStatus);
+    void setAimLineRotation(int aimLineRotation);
+    void setRubberBandRotation(int rubberBandRotation);
+    void setUnitIndex(int unitIndex);
+    void setIsCampaign(bool isCampaign);
+    void setMapFile(const QString &mapFile);
+    void setEffectsContainer(const QList<QObject *> &effectsContainer);
+    void setOrderMarkersContainer(const QList<QObject *> &orderMarkersContainer);
+    void setUnitGroups(const QList<QObject *> &unitGroups);
+    void setZoom(qreal zoom);
+    void setZoomPoint(const QPoint &zoomPoint);
+    void setPaused(bool paused);
+    void setMenuBackgroundColor(const QColor &menuBackgroundColor);
+
+signals:
+    void scenarioFileChanged();
+    void scenarioWinStatusChanged();
+    void aimLineRotationChanged();
+    void rubberBandRotationChanged();
+    void unitIndexChanged();
+    void isCampaignChanged();
+    void mapFileChanged();
+    void effectsContainerChanged();
+    void orderMarkersContainerChanged();
+    void unitGroupsChanged();
+    void zoomChanged();
+    void zoomPointChanged();
+    void pausedChanged();
+    void menuBackgroundColorChanged();
+
+private:
+    QString m_scenarioFile;
+    QString m_scenarioWinStatus;
+    int m_aimLineRotation;
+    int m_rubberBandRotation;
+    int m_unitIndex;
+    bool m_isCampaign;
+    QString m_mapFile;
+    QList<QObject *> m_effectsContainer;
+    QList<QObject *> m_orderMarkersContainer;
+    QList<QObject *> m_unitGroups;
+    qreal m_zoom;
+    QPoint m_zoomPoint;
+    bool m_paused;
+    QColor m_menuBackgroundColor;
 };
 
 #endif // CCFQMLBASESCENARIO_H
